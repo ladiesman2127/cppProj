0541d39f36b8ed944be3a2627afa8dddc657d621
AustinFreeman <rekkles_fnc1@outlook.com>
2022-04-07 09:45:50 +0300

    master

--- file_streams/file_streams.cpp
+++ file_streams/file_streams.cpp
@@ -1,12 +1,52 @@
 ﻿#include <fstream>
 #include <iostream>
+#include <string>
+#include <Windows.h>
 
+
+class point
+{
+private:
+	int x, y, z;
+public:
+	point(int x, int y, int z)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+	point()
+	{
+		x = y = z = 0;
+	}
+
+};
+
+std::ostream &operator << (std::ostream &os, const point &p)
+{
+
+	return os;
+}
 int main()
 {
-	std::ofstream fout;
-	fout.open("file.txt", std::ofstream::app);
-	fout << "GG!   asdasd asd ";
-	fout.close();
+	/*std::ifstream fin;
+	fin.open("file.txt");
+	std::string str;
+	while (!fin.eof())
+	{
+		std::getline(fin, str);
+		std::cout << str << '\n';
+	}
+	fin.close();*/
+	/*std::ofstream fout;
 
-	return 0;
+	point a(12321,5435,777);
+	fout.open("file.txt");
+	fout.write((char*)&a,sizeof(a));
+	fout.close();*/
+	std::fstream fs;
+	fs.open("file.txt", std::fstream::in | std::fstream::out | std::fstream::app);
+
+
+		return 0;
 }
