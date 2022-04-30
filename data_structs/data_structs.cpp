fd4acb00c8d18d778964280f5001e96ebf23f6be
AustinFreeman <rekkles_fnc1@outlook.com>
2022-04-30 16:59:47 +0300

    master

--- data_structs/data_structs.cpp
+++ data_structs/data_structs.cpp
@@ -10,7 +10,7 @@ private:
 	public:
 		node* ptr_next; // adress of next node
 		T data;
-		node(T data = T(), node* ptr_next = nullptr) 
+		node(T data = T(), node* ptr_next = nullptr)
 		{
 			this->data = data;
 			this->ptr_next = ptr_next;
@@ -23,7 +23,15 @@ public:
 	my_list();
 	~my_list();
 	void push_back(T data);
-	int get_size() { return size; }
+	void push_front(T data);
+	void pop_back(T data);
+	void pop_front();
+	void remove_at(int index);
+	void insert(T data, int index);
+	int get_size() const { return size; }
+
+	void clear();
+	T& operator[](const int index);
 };
 
 template <class T>
@@ -36,7 +44,7 @@ my_list<T>::my_list()
 template <class T>
 my_list<T>::~my_list()
 {
-		
+	clear();
 }
 
 template <class T>
@@ -54,15 +62,117 @@ void my_list<T>::push_back(T data)
 	}
 	else
 	{
-		node* current = this->head ;
-		while(current->ptr_next != nullptr)
+		node* cur_node = this->head;
+		while(cur_node->ptr_next != nullptr)
 		{
-			current = current->ptr_next;
+			cur_node = cur_node->ptr_next;
 		}
-		current->ptr_next = new node(data);
+		cur_node->ptr_next = new node(data);
 	}
 
 	size++; // Увеличиваем size после добавления элемента
+}
+
+template <class T>
+void my_list<T>::push_front(T data)
+{
+	head = new node(data, head);
+	size++;
+}
+
+template <class T>
+void my_list<T>::pop_back(T data)
+{
+	node* cur_node = this->head;
+
+	for(int i = 0; i < size - 1; i++)
+	{
+		cur_node = cur_node->ptr_next;
+	}
+	delete cur_node->ptr_next;
+	cur_node->ptr_next = nullptr;
+}
+
+template <class T>
+void my_list<T>::insert(T data, int index)
+{
+	if(index == 0)
+	{
+		push_front(data);
+	}
+	else
+	{
+		node* previous = this->head;
+		for(int i = 0; i < index - 1; i++)
+		{
+			previous = previous->ptr_next;
+		}
+
+		node* new_node = new node(data, previous->ptr_next);
+		previous->ptr_next = new_node;
+		size++;
+	}
+}
+
+
+template <class T>
+void my_list<T>::pop_front()
+{
+	//Запоминаем адресс головы
+	node* temp = head;
+	//голову задаем за вторую ноду
+	head = head->ptr_next;
+	//удаляем данные с прежней головы
+	delete temp;
+	size--;
+}
+
+template <class T>
+void my_list<T>::remove_at(int index)
+{
+	if(index == 0)
+	{
+		pop_front();
+	}
+	else if(index == size - 1)
+	{
+		pop_back();
+	}
+	else
+	{
+		node* cur_node = this->head;
+		for(int i = 0;i < index - 1;i++)
+		{
+			cur_node = cur_node->ptr_next;
+		}
+		node* temp = cur_node->ptr_next;
+		cur_node->ptr_next = cur_node->ptr_next->ptr_next;
+		delete temp;
+		size--;
+	}
+}
+
+template <class T>
+void my_list<T>::clear()
+{
+	while(size)
+	{
+		pop_front();
+	}
+}
+
+template <class T>
+T& my_list<T>::operator[](const int index)
+{
+	int cur_index = 0;
+	node* cur_node = this->head;
+	while(cur_node != nullptr)
+	{
+		if(cur_index == index)
+			return cur_node->data;
+		cur_index++;
+		cur_node = cur_node->ptr_next;
+	}
 }
 
 int main()
@@ -71,5 +181,10 @@ int main()
 	lst.push_back(1);
 	lst.push_back(11);
 	lst.push_back(72);
+	lst.insert(2, 2);
+	for(int i = 0; i < lst.get_size(); i++)
+	{
+		std::cout << lst[i] << " ";
+	}
 	return 0;
 }
---
+++ interview/interview.cpp
@@ -0,0 +1,45 @@
+﻿
+
+#include <iostream>
+
+void Merge(int* a, int p, int q, int r)
+{
+	int i = p - 1, j = q + 1;
+	std::cout << p << " " << q << '\n';
+	while(i < q && j < r)
+	{
+		if(a[i] > a[j])
+		{
+			//std::cout <<"before: " << a[i] << " " << a[j] << '\n';
+			std::swap(a[i], a[j]);
+			//std::cout <<"after: " << a[i] << " " << a[j] << '\n';
+			i++;
+		}
+		else
+		{
+			j++;
+		}
+	}
+
+}
+
+void sort(int* a, int p, int r)
+{
+	if(p < r)
+	{
+		int q = (p + r) / 2;
+		sort(a, p, q);
+		sort(a, q + 1, r);
+		Merge(a, p, q, r);
+
+	}
+}
+int main()
+{
+	int a[] { 5, 2, 4, 6, 1, 3,6,2,5 };
+	sort(a, 0, sizeof(a) / sizeof(a[0]));
+	for(int i = 0; i < sizeof(a) / sizeof(a[0]); i++)
+	{
+		std::cout << a[i] << " ";
+	}
+}
